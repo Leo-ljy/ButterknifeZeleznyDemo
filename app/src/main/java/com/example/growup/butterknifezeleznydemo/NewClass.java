d49c47bcb3dd1547c3d9b47fa237aa50cd4f9ae5
1521541979 <1521541979@qq.com>
2016-06-23 18:28:16 +0800

    这是我新添加的一个类

---
+++ app/src/main/java/com/example/growup/butterknifezeleznydemo/NewClass.java
@@ -0,0 +1,12 @@
+package com.example.growup.butterknifezeleznydemo;
+
+/**
+ * 作者：        追梦
+ * 邮箱：        1521541979@qq.com
+ * 公司：        杭州码友网络科技有限公司
+ * 日期：        2016/6/23 18:27
+ * 描述：
+ */
+public class NewClass
+{
+}
